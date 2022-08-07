b543867f85bd82c323cbdc2f7949d604b8ada8fb
rohitvishin <rohitvishwakarma65@gmail.com>
2022-08-07 14:05:31 +0530

    Update

--- src/screen/Updater.jsx
+++ src/screen/Updater.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col,Form,Button } from "react-bootstrap";
+
 export default function Updater(){
     const handleSubmit = (event) => {
         console.log("in")
@@ -6,9 +9,19 @@ export default function Updater(){
         console.log(event.target[0].value)
     }
     return(
-        <form onSubmit={handleSubmit}>
-            <input type="text" value="new"/>
-            <input type="submit" value="submit"/>
-        </form>
+        <Container>
+            <Row>
+                <Col>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Control type="text" name="name" placeholder="Enter Name here" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Button type="submit" name="submit"> sumit</Button>
+                    </Form.Group>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
