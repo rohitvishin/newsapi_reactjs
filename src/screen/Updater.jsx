e3f352e6b9ef71569c069a380dae7670caf75f44
rohitvishin <rohitvishwakarma65@gmail.com>
2022-08-10 22:26:23 +0530

    mysql insert operation

--- src/screen/Updater.jsx
+++ src/screen/Updater.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container,Row,Col,Form,Button } from "react-bootstrap";
+import axios from "axios";
 
 export default function Updater(){
     const handleSubmit = (event) => {
-        console.log("in")
         event.preventDefault();
-        console.log(event.target[0].value)
+        const formData = new FormData();
+        formData.append('title',event.target[0].value);
+        formData.append('description',event.target[1].value);
+        axios.post('http://localhost/newsapi/updateNews.php',formData).then(res => {
+        if(res)
+        console.log("successful");
+        else
+        console.log("operation failed");
+      })
     }
     return(
         <Container>
             <Row>
                 <Col>
+                <h2>Add Content</h2>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group>
-                        <Form.Control type="text" name="name" placeholder="Enter Name here" />
+                        <Form.Control type="text" name="title" className="formcss" placeholder="Enter Name here" />
                     </Form.Group>
+                    <Form.Control as="textarea" className="formcss" name="description" placeholder="Enter Description Here"/>
+                    <Form.Control type="file" name="image" className="formcss" placeholder="Choose File"/>
                     <Form.Group>
-                        <Button type="submit" name="submit"> sumit</Button>
+                        <Button type="submit" name="submit" className="formcss"> sumit</Button>
                     </Form.Group>
                 </Form>
                 </Col>
